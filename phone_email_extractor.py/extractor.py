cb576524548000f1eaee601cba87ed392b3d14e4
Michal Kwiatek <michal.kwiatek@nokia.com>
2022-01-12 14:24:52 +0100

    email regex

--- phone_email_extractor.py/extractor.py
+++ phone_email_extractor.py/extractor.py
@@ -3,10 +3,16 @@ import pyperclip
 
 
 PHONE_NUMBER_REGEX = re.compile(r'''(
-    (\+48)?          # Polish
-    (\d{3})          # 1st 3 digits
-    (\s|-)?          # separator
-    (\d{3})          # 2nd 3 digits
-    (\s|-)?          # separator
-    (\d{3})          # 3rd 3 digits
+    (\+48)?                # Polish country code
+    (\d{3})                # 1st 3 digits
+    (\s|-)?                # separator
+    (\d{3})                # 2nd 3 digits
+    (\s|-)?                # separator
+    (\d{3})                # 3rd 3 digits
+    )''', re.VERBOSE)
+EMAIL_REGEX = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+      # username
+    @                      # @ symbol
+    [a-zA-Z0-9.-]+         # domain name
+    (\.[a-zA-Z]{2,4})      # dot-something
     )''', re.VERBOSE)
