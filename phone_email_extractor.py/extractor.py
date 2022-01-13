fca28754e06c1a3787436edc543b1f7deddfc0e4
Michal Kwiatek <michal.kwiatek@nokia.com>
2022-01-13 12:01:18 +0100

    Script written in procedural manner

--- phone_email_extractor.py/extractor.py
+++ phone_email_extractor.py/extractor.py
@@ -1,3 +1,11 @@
+"""
+Script that extract all phone numbers and email addresses from text.
+
+How to use: 
+1. CTRL+C to copy text to lookup.
+2. Execute the program.
+3. CTRL+V will paste all findings. 
+"""
 import re
 import pyperclip
 
@@ -16,3 +24,21 @@ EMAIL_REGEX = re.compile(r'''(
     [a-zA-Z0-9.-]+         # domain name
     (\.[a-zA-Z]{2,4})      # dot-something
     )''', re.VERBOSE)
+
+ # Find matches in clipboard text.
+text = str(pyperclip.paste())
+
+matches = []
+for groups in PHONE_NUMBER_REGEX.findall(text):
+       phone_num = '-'.join([groups[2], groups[4], groups[6]])
+       matches.append(phone_num)
+for groups in EMAIL_REGEX.findall(text):
+       matches.append(groups[0])
+
+# Copy results to the clipboard.
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
