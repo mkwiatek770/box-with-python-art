53f57f493f88736cc030f8e2215778b443516be1
Michal Kwiatek <michalakwiatek@gmail.com>
2022-01-17 10:25:10 +0100

    new script description

---
+++ clipboard_aggregaor/b_generator.py
@@ -0,0 +1,9 @@
+"""
+Script which compress folder (or list of folders) at given location.
+
+The name of zipped folder would be: <date>_<hour>_<folder_name>.zip
+date: DD-MM-YYYY format
+hour: HH-MM format
+folder_name: name of folder(s) at given location,
+             for many folders pattern would be '<f1>&<f2>&<f3>'
+"""
--- clipboard_aggregaor/cpb_aggregator.py
+++ clipboard_aggregaor/cpb_aggregator.py
@@ -2,3 +2,4 @@
 Script which enables to aggregate copied content.
 Also, there's possibility to save clipboard content into files.
 """
+# https://stackoverflow.com/questions/11918999/key-listeners-in-python (pynput, cursor, keyboard)
