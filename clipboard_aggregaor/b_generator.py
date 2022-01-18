e753a47e83ace5ab2f62a8ecdc1a46b0ec11e1bb
Michal Kwiatek <michalakwiatek@gmail.com>
2022-01-18 11:34:08 +0100

    cleanup

--- clipboard_aggregaor/b_generator.py
+++ clipboard_aggregaor/b_generator.py
@@ -10,7 +10,6 @@ folder_name: name of folder(s) at given location,
 from datetime import datetime
 import pathlib
 import zipfile
-import os
 
 
 class InvalidLocationError(Exception):
@@ -21,9 +20,9 @@ class IsNotDirectoryError(Exception):
 
 
 def compress(*locations) -> None:
-    # wyciagnij wszystkie sciezki # TODO: create validation decorator
+    # TODO: create validation decorator
     paths = [pathlib.Path(location) for location in locations]
-    # zamien na pathlib.Path zweryfikuj istnienie
+
     for path in paths:
         # wyjatki
         if not path.exists():
@@ -31,20 +30,12 @@ def compress(*locations) -> None:
         elif path.is_file():
             raise IsNotDirectoryError(f'Given path: {path} is not a directory')
 
-    # stworz nazwe output zip file
     cwd = pathlib.Path().resolve()
     timestamp = datetime.now().strftime("%d-%m-%Y_%H:%M")
     zip_folder_name = '&'.join(path.name for path in paths)
     full_path = cwd / f'{timestamp}_{zip_folder_name}.zip'
 
-    # otworz ctx magerem zip obj
     with zipfile.ZipFile(full_path, 'w') as zip_file:
-        # przejdz po wszystkich podfolderach kazdego fold i zrob obj.write
         for path in paths:
             for file in path.rglob('*'):
                 zip_file.write(file, path.name / file.relative_to(path))
-        # obsluga wyjatkow
-
-
-if __name__ == '__main__':
-    print(compress('/home/mkwiatek/Desktop/temp', '/home/mkwiatek/Desktop/logs/temp2'))
