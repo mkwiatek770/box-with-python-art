28d6bd82d383206c0bbbbc7bd3218c4a9ffdc6ec
Michal Kwiatek <michalakwiatek@gmail.com>
2022-02-01 14:14:03 +0100

    simple parser

--- web_scraping/simple_parser.py
+++ web_scraping/simple_parser.py
@@ -4,6 +4,9 @@ import requests
 
 
 LINK = 'https://stawiszynski.org/podcasty/skadinad/'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
+}
 
 res = requests.get(LINK)
 if res.status_code == 200:
