ca710f861ca9652691d6f3c785114b3d475677f8
Michal Kwiatek <michalakwiatek@gmail.com>
2022-02-01 14:35:13 +0100

    all h2 titles

--- web_scraping/parse_few_pages.py
+++ web_scraping/parse_few_pages.py
@@ -12,5 +12,9 @@ async def get_page(page_num: int = 1):
 
 
 if __name__ == '__main__':    
-    page_nums = list(range(1, 23))
-    result = asession.run( *[lambda page_num=page_num: get_page(page_num) for page_num in page_nums])
+    page_nums = list(range(2, 3))
+    results = asession.run( *[lambda page_num=page_num: get_page(page_num) for page_num in page_nums])
+
+    for result in results:
+        for h2 in result.html.find(".article h2"):
+            print(h2.text)
