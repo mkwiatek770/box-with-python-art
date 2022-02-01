aa0dcb2a9bddfc49a4bc2fa8c97a19d93a6b08e2
Michal Kwiatek <michalakwiatek@gmail.com>
2022-02-01 14:52:17 +0100

    parse h2 texts

--- web_scraping/parse_few_pages.py
+++ web_scraping/parse_few_pages.py
@@ -1,7 +1,10 @@
+from collections import defaultdict
 from requests_html import AsyncHTMLSession
 
 
 LINK = 'https://www.polskieradio.pl/7/5102/Strona'
+IGNORED_TEXTS = ('Dorota Truszczak', 'Historia żywa')
+ARTICLES = defaultdict(list)
 asession = AsyncHTMLSession()
 
 
@@ -11,10 +14,20 @@ async def get_page(page_num: int = 1):
     return await asession.get(url)
 
 
+def parse_titles(result):
+    if result.status_code == 200:
+        page_num = int(result.html.url.split(result.html.base_url)[1])
+        for h2 in result.html.find(".article h2"):
+            text = h2.text
+            if h2.text not in IGNORED_TEXTS:
+                ARTICLES[page_num].append(h2.text)
+
+
 if __name__ == '__main__':    
-    page_nums = list(range(2, 3))
+    page_nums = list(range(1, 2))
     results = asession.run( *[lambda page_num=page_num: get_page(page_num) for page_num in page_nums])
 
     for result in results:
-        for h2 in result.html.find(".article h2"):
-            print(h2.text)
+        parse_titles(result)
+
+    print(ARTICLES)
